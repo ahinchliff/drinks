2464388ba218f8e66c12605a52a6fdb94cd25e24
ahinchliff <anthony.hinchliff@gmail.com>
2017-10-15 19:22:33 +0100

    adding googlemaps api key for production

--- src/components/event_profile/MapArea.jsx
+++ src/components/event_profile/MapArea.jsx
@@ -6,7 +6,7 @@ function MapArea(props) {
     <div>
       <span style={{fontFamily: '"Helvetica", sans-serif', fontSize: '18px'}}>Location</span>
       <Map
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_5FdODt2U1KVnKhawWlk1fAUPFiI4rBE&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `500px`, marginTop: '10px' }} />}
         mapElement={<div style={{ height: `100%` }} />}
